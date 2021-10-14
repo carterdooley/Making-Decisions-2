628df31dfbf1d4bbda7a5bd6b1d84811a0f12dbc
carter dooley <carter.j.dooley43@gmail.com>
2021-10-14 11:33:08 -0500

    Finished Problem 2

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -7,15 +7,16 @@ var arr = [10,20,30,40,50,60]
 */
 
 //Code Here
-
-
+let firstItem = arr[0]
+console.log(firstItem)
 ////////// PROBLEM 2 //////////
 /*
   Remove the last item from the 'arr' array and store it in a variable named 'lastItemRemoved'. Console.log the array to verify you no longer have the last item.
 */
 
 //Code Here
-
+let lastItemRemoved = arr.pop()
+console.log(lastItemRemoved)
 ////////// PROBLEM 3 //////////
 
 // Do not edit the code below.
